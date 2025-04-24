a5906cc343c5ac3eebf1b4e09a7b67baff2ec7b5
devinda.jay <devinda.jay.me@gmail.com>
2025-04-24 13:36:24 +0530

    created BookResource class with API

--- src/main/java/com/mycompany/bookstore/resource/BookResource.java
+++ src/main/java/com/mycompany/bookstore/resource/BookResource.java
@@ -5,6 +5,7 @@
 package com.mycompany.bookstore.resource;
 
 import com.mycompany.bookstore.exception.AuthorNotFoundException;
+import com.mycompany.bookstore.exception.BookNotFoundException;
 import com.mycompany.bookstore.exception.InvalidInputException;
 import com.mycompany.bookstore.model.Book;
 import com.mycompany.bookstore.storage.InMemoryStore;
@@ -40,11 +41,49 @@ public class BookResource {
             store.addBook(book);
             URI location = UriBuilder.fromPath("/api/books/{id}").resolveTemplate("id", book.getId()).build();
             return Response.created(location).build();
+            
         } catch (InvalidInputException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+            
         } catch (AuthorNotFoundException e) {
             return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
         }
     }
+    
+    @GET
+    @Path("{/id}")
+    
+    public Response updateBook(@PathParam("id") int id, Book book){
+        
+        if(book.getId() != id){
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Book ID in path (" + id + ") does not match Book ID in body (" + book.getId() + ")")
+                    .build();
+        }
+        try {
+            store.updateBook(id, book);
+            return Response.noContent().build();
+            
+        } catch (BookNotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+            
+        } catch (InvalidInputException | AuthorNotFoundException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
     }
+    
+    @DELETE
+    @Path("/{id}")
+    
+    public Response deleteBook(@PathParam("id") int id) {
+        
+        try {
+            store.deleteBook(id);
+            return Response.noContent().build();
+            
+        } catch (BookNotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        }
+    }
+  }
 
